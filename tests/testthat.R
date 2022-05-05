e241a65103cd283e604e6da718bba210503d3bc6
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2022-05-05 06:00:06 +0000

    version 1.6.6

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(BIGL)
 
+#if (Sys.getenv("JENKINS_URL") != "")
+#  options(testthat.default_check_reporter = "junit", testthat.output_file = "results.xml")
+
 test_check("BIGL")
